a416f527b6ee4e5f2fabca0eba64cf0e1ceac7eb
Riku Sahu <riku.sahu237@gmail.com>
2025-10-26 16:13:29 +0530

    Initialize 1st_program.py with print statements and comments
    Added comments and a multi-line string to the first program.

---
+++ 01_modules_comments_pip_ps/1st_program.py
@@ -0,0 +1,28 @@
+print("Hello World")
+# for runing Terminal -> python file_name
+# install packege like flask -> pip install flask
+# install packege pyjoke -> pip install pyjokes
+
+"""multi line commend
+1st line
+2nd line
+and so ..
+"""
+'''this also valid
+1st 
+2nd
+and so..
+'''
+print('''Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.
+
+Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are''')
