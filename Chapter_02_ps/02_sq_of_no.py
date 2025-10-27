904035584ed65c0cd0cbd45abe0445f624559bf6
Riku Sahu <riku.sahu237@gmail.com>
2025-10-27 22:24:53 +0530

    Add files via upload

---
+++ Chapter_02_ps/02_sq_of_no.py
@@ -0,0 +1,5 @@
+# 6. Write a python program to calculate the square of a number entered by the user.
+a = int(input("Enter number a : "))
+
+print("Square of number enter is ", a**2)
+print("Square of number enter is ", a*a)
