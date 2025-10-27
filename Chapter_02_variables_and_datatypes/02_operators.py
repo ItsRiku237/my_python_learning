969c5f10f5b78e0c06ecf960b26b88ba6a6a1dbb
Riku Sahu <riku.sahu237@gmail.com>
2025-10-27 22:19:24 +0530

    Add files via upload

---
+++ Chapter_02_variables_and_datatypes/02_operators.py
@@ -0,0 +1,20 @@
+# comparison operators
+ricz_237 = 5<3
+ri_ku = 5>1
+print(ricz_237)
+print(ri_ku)
+
+# Logical operators
+# Truth table of 'or'
+print("True or False is ", True or False)
+print("True or True is ", True or False)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of 'and'
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print(not(True))
