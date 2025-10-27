8eee983518040ed3e261d4df22f19de6d82208e4
Riku Sahu <riku.sahu237@gmail.com>
2025-10-27 22:20:43 +0530

    Add files via upload

---
+++ Chapter_02_variables_and_datatypes/04_input.py
@@ -0,0 +1,15 @@
+# Write a python program to add two numbers. 
+
+a = input("Enter number 1 : ")
+b = input("Enter number 2 : ")
+print("Number a is : ",a)
+print("Number b is : ",b)
+print("Sum is ", a+b)
+# it gone to concatinate not add.
+# bcz when we take input it store as a string.
+
+c = int(input("Enter number c : "))
+d = int(input("Enter number d : "))
+print("Number c is : ",c)
+print("Number d is : ",d)
+print("Sum is ", c+d)
