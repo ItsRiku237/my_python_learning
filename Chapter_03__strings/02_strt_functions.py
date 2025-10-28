0defcfb045a52d3b13a1a85eb9447818e2945190
Riku Sahu <riku.sahu237@gmail.com>
2025-10-28 20:04:45 +0530

    Add files via upload

---
+++ Chapter_03__strings/02_strt_functions.py
@@ -0,0 +1,19 @@
+colName = "government college of engineering"
+
+# 1
+print(len(colName))
+# 2
+print(colName.startswith("Gov"))
+# 3
+print(colName.endswith("ing"))
+# 4
+print(colName.capitalize())
+# 5
+count = colName.count("r")
+print(count)
+# 6
+index = colName.find("nt")
+print(index)
+# 7
+replaceString = colName.replace("college","IIT")
+print(replaceString)
