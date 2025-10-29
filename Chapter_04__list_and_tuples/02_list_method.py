e6855c1b374a6361c5d0d097326cbd9fe7b872b4
Riku Sahu <riku.sahu237@gmail.com>
2025-10-29 21:21:40 +0530

    Add files via upload

---
+++ Chapter_04__list_and_tuples/02_list_method.py
@@ -0,0 +1,20 @@
+l1 = [1,6,32,96,4,11]
+
+l1.sort()
+print(l1)
+
+l1.reverse()
+print(l1)
+
+l1.insert(3,233333)
+print(l1)
+
+# l1.pop(3)
+print(l1.pop(3))# print delete element.
+print(l1)# in here index 3 element got delete
+
+l1.append(41)
+print(l1)
+
+l1.remove(11)
+print(l1)#remove element 11 from list
