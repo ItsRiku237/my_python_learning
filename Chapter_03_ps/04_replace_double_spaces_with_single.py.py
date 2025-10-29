3cab06027e44e59f0cb33ee9ae740e025c9ab11c
Riku Sahu <riku.sahu237@gmail.com>
2025-10-29 21:18:49 +0530

    Add files via upload

---
+++ Chapter_03_ps/04_replace_double_spaces_with_single.py.py
@@ -0,0 +1,7 @@
+# 4. Replace the double space from problem 3 with single spaces.
+# this is a string.
+# and string are immutable data type in python.
+str = "Riku is a good  boy. "
+
+print(str.replace("  "," "))
+print(str) # string are immutable .
