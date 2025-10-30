75f78b28f021d9c89e1c3fbebecb10dc02458dbb
Riku Sahu <riku.sahu237@gmail.com>
2025-10-30 21:51:37 +0530

    Add files via upload

---
+++ Chapter_04_ps/04_sum_of_list_numbers.py.py
@@ -0,0 +1,10 @@
+# 4. Write a program to sum a list with 4 numbers. 
+
+x = [10, 20, 30 , 40]
+
+#method 1
+print(sum(x))# sum function add integer present in the list.
+
+#method 2
+a, b ,c, d = x
+print(a+b+c+d)
