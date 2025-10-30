f284439121be1a3add60c13a3c71fc150d96eeaa
Riku Sahu <riku.sahu237@gmail.com>
2025-10-30 21:45:21 +0530

    Add files via upload

---
+++ Chapter_04__list_and_tuples/03_tuple.py
@@ -0,0 +1,11 @@
+# tuple with only one element needs a comma 
+
+a = (2, 3 ,23, 45 ,False, "Roshan" ,'shiba')
+b = (1)
+c = (2,)# tuple with only one element needs a comma 
+
+print(a)
+print(type(a))
+print(type(b))
+print(type(c))
+print(b)
