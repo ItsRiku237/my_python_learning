16ad78da37658b0ad99e1db6820e183b317fce1c
Riku Sahu <riku.sahu237@gmail.com>
2025-10-30 21:47:46 +0530

    Create 01_store_fruits_in_list.py

---
+++ Chapter_04_ps/01_store_fruits_in_list.py
@@ -0,0 +1,22 @@
+# 1. Write a program to store seven fruits in a list entered by the user. 
+
+Fruits = []
+f1 = input("Enter Fruits name :")
+Fruits.append(f1)
+f2 = input("Enter Fruits name :")
+Fruits.append(f2)
+f3 = input("Enter Fruits name :")
+Fruits.append(f3)
+f4 = input("Enter Fruits name :")
+Fruits.append(f4)
+f5 = input("Enter Fruits name :")
+Fruits.append(f5)
+f6 = input("Enter Fruits name :")
+Fruits.append(f6)
+f7 = input("Enter Fruits name :")
+Fruits.append(f7)
+print (Fruits)
+
+# wrong concept
+# Fruits = list(input("Enter seven fruit name : "))
+# print(Fruits)
