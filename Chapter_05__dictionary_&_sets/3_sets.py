67632ea1ee275292ff35a79abb507590b7c2d5cc
Riku Sahu <riku.sahu237@gmail.com>
2025-10-31 21:52:27 +0530

    Add files via upload

---
+++ Chapter_05__dictionary_&_sets/3_sets.py
@@ -0,0 +1,14 @@
+s = {1, 25, 3, 89 , 1, 3, 3, 3 ,98 , "Riku"}
+# in set element are not repeat.
+ 
+print(type(s))
+print(s)
+
+d = {} #empty dictionary
+print(type(d))
+
+e_t = () #empty touple
+print(type(e_t))
+
+e_s = set() #empty set
+print(type(e_s))
