dc0d7587ac4fcea91df173ee195d5443c91fb2d4
Riku Sahu <riku.sahu237@gmail.com>
2025-11-06 20:20:46 +0530

    Add files via upload

---
+++ Chapter_8__functions_&_recursions/2_function_with_argument.py
@@ -0,0 +1,10 @@
+#FUNCTIONS WITH ARGUMENTS
+
+def goodDay(name,ending):
+    print("Good Day, "+ name)
+    print(ending)
+    return "Done !!"
+
+a = goodDay("Riku","Thank you")
+print(a)
+goodDay("Harry","Thanks")
