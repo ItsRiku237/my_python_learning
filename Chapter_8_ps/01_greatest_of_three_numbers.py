1b98d0afe079d97ec3351d54c7814ade17e7e510
Riku Sahu <riku.sahu237@gmail.com>
2025-11-07 20:10:19 +0530

    Add function to find greatest of three numbers

---
+++ Chapter_8_ps/01_greatest_of_three_numbers.py
@@ -0,0 +1,17 @@
+'''1. Write a program using functions to find greatest of three numbers. '''
+
+def greatest(a,b,c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and b>c):
+        return b
+    else:
+        return c
+
+
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+c = int(input("Enter 3rd number : "))
+
+g = greatest(a, b ,c)
+print("greatest number is : ",+g)
