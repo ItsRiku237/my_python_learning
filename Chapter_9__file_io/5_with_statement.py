9fc8afd42188fc8ab495a3ee86f32bf3534a906f
Riku Sahu <riku.sahu237@gmail.com>
2025-11-09 22:32:31 +0530

    Add files via upload

---
+++ Chapter_9__file_io/5_with_statement.py
@@ -0,0 +1,12 @@
+f = open("file.txt") # by default 'r' is there
+# f = open("file.txt", "r")
+print(f.read())
+f.close()
+
+# we can write this satement as like this :
+
+with open("file.txt" , "r") as f:
+    print(f.read())
+    
+
+# we dont have to explicitly close the file. when we use "with" statement. 
